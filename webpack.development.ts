75c75a2dce245d121226b308241879cec090c058
ktsmon <43135441+pestobaimon@users.noreply.github.com>
2021-04-20 14:07:44 +0700

    Initial commit

---
+++ webpack.development.ts
@@ -0,0 +1,55 @@
+import * as webpack from "webpack";
+import * as path from "path";
+
+import fs from "fs";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const pkg: any = JSON.parse(fs.readFileSync(path.join(__dirname, "package.json"), "utf-8"));
+
+module.exports = (env: { mode: "development" | "production" }) => {
+    const devConfig = {
+        mode: env.mode,
+
+        devtool: "inline-source-map",
+
+        devServer: {
+            host: "0.0.0.0",
+            open: true,
+            openPage: "http://localhost:8080/",
+        },
+
+        module: {
+            rules: [
+                {
+                    enforce: "pre",
+                    test: /\.(js|jsx|ts|tsx)$/,
+                    exclude: /node_modules/,
+                    loader: "eslint-loader",
+                },
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "[name].js"
+        },
+
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+            }),
+
+            new webpack.DefinePlugin({
+                VERSION: JSON.stringify(pkg.version + "dev"),
+            }),
+        ],
+    };
+
+    return devConfig;
+};
---
+++ webpack.production.ts
@@ -0,0 +1,72 @@
+import * as path from "path";
+import * as webpack from "webpack";
+
+import fs from "fs";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import OptimizeCssAssetsPlugin from "optimize-css-assets-webpack-plugin";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const pkg: any = JSON.parse(fs.readFileSync(path.join(__dirname, "package.json"), "utf-8"));
+
+module.exports = (env: { mode: "development" | "production" }) => {
+    return {
+        mode: env.mode,
+
+        module: {
+            rules: [
+                {
+                    enforce: "pre",
+                    test: /\.(js|jsx|ts|tsx)$/,
+                    exclude: /node_modules/,
+                    loader: "eslint-loader",
+                    options: {
+                        emitError: true,
+                        emitWarning: true,
+                        failOnError: true,
+                        failOnWarning: true,
+                    },
+                },
+                {
+                    test: /\.(js|jsx|ts|tsx)$/,
+                    use: [
+                        {
+                            loader: "babel-loader",
+                        },
+                    ],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "game.[contenthash].js",
+        },
+
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "[name].[contenthash].css",
+            }),
+
+            new OptimizeCssAssetsPlugin({
+                assetNameRegExp: /\.css$/i,
+                cssProcessor: require("cssnano"),
+                cssProcessorPluginOptions: {
+                    preset: ["default", { discardComments: { removeAll: true } }],
+                },
+                canPrint: true,
+            }),
+
+            new webpack.DefinePlugin({
+                VERSION: JSON.stringify(pkg.version + "r"),
+            }),
+
+            new webpack.ProgressPlugin(),
+        ],
+    };
+};
